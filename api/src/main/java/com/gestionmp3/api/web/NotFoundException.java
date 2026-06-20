fb517f9f85980d3a9bd4346d26a9806498a5aba7
bruel <clarita.razafiarisoa@gmail.com>
2026-06-20 18:32:20 +0300

    maj

--- api/src/main/java/com/gestionmp3/api/web/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gestionmp3.api.web;
-
-/** Levee quand une ressource demandee n'existe pas (renvoie un HTTP 404). */
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
